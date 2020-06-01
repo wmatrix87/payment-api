aea560d6c7a169262eab7a2038bbbb1eeac3ddb7
Serhii Yakushov <serhii.yakushov@right-now-group.de>
2020-06-01 16:19:10 +0300

    err names

--- controllers/authorization.js
+++ controllers/authorization.js
@@ -3,14 +3,20 @@ const redisClient = require('./signin').redisClient;
 const requireAuth = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization) {
-    return res.status(401).send('Unauthorized');
+    return res.status(401).json({
+      code: 'ERR_UNAUTHORIZED',
+      message: 'No auth token provided',
+    });
   }
   
   const token = authorization.startsWith('Bearer ') ? authorization.substring(7, authorization.length) : authorization;
 
   return redisClient.get(token, (err, reply) => {
     if (err || !reply) {
-      return res.status(401).send('Unauthorized');
+      return res.status(401).json({
+        code: 'ERR_AUTH_TOKEN_EXPIRED',
+        message: 'Auth token expired',
+      });
     }
     return next();
   });
